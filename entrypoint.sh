5c2dda9fad5c22100b1b423f4857cb03dc694b74
Arun Kumar <87801852+arunrana1214@users.noreply.github.com>
2025-08-25 15:06:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -99,7 +99,7 @@ conjur_authn() {
 		echo "::debug Authenticate via Authn-JWT"
         JWT_TOKEN=$( curl -s -H "Authorization:bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL" | jq -r .value )
         handle_git_jwt "$JWT_TOKEN"
-		echo $JWT_TOKEN
+		echo "$JWT_TOKEN"
         
 		if [[ -n "$INPUT_CERTIFICATE" ]]; then
             echo "::debug Authenticating with certificate"
